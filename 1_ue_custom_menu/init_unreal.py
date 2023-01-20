7ba8d83e2c9e154014d8a8acded98e01d495f240
Jiri Miratsky <info@ikoon.cz>
2023-01-20 13:27:24 +0100

    Update init_unreal.py

--- 1_ue_custom_menu/init_unreal.py
+++ 1_ue_custom_menu/init_unreal.py
@@ -7,3 +7,8 @@ importlib.reload(init_menu)
 # ------------ create menu ---------------
 if __name__ == '__main__':
     init_menu.create_menu()
+
+
+# ------------ lower idle GPU usage ------------
+# ------------   (not to be 100%)   ------------
+unreal.SystemLibrary.execute_console_command(None, "t.MaxFPS 60")
